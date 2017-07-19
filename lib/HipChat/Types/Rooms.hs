f304640ce8eeb9ec96e13b5218fe816b082852d4
Oswyn Brent <obrent@atlassian.com>
2017-07-19 21:39:37 +1000

    add remaining fields to card type

--- lib/HipChat/Types/Rooms.hs
+++ lib/HipChat/Types/Rooms.hs
@@ -23,6 +23,9 @@ module HipChat.Types.Rooms
   , NotificationColor(..)
   , SendNotificationRequest(..)
   , MessageFormat(..)
+  , CompoundDescription(..)
+  , Description(..)
+  , Thumbnail(..)
   , Card(..)
   , CardAttribute(..)
   , CardAttributeValue(..)
@@ -135,14 +138,54 @@ data CardAttribute = CardAttribute
 instance ToJSON CardAttribute where
   toEncoding = genericToEncoding (aesonDrop 0 snakeCase){omitNothingFields = True}
 
+data Description = Description
+  { value  :: Text
+  , format :: MessageFormat
+  } deriving (Generic, Show)
+
+instance ToJSON Description where
+  toEncoding = genericToEncoding (aesonDrop 0 snakeCase){omitNothingFields = True}
+
+newtype CompoundDescription = CompoundDescription
+  { unCompoundDescription :: Either Text Description
+  } deriving Show
+
+instance ToJSON CompoundDescription where
+  toJSON (CompoundDescription (Left  x)) = toJSON x
+  toJSON (CompoundDescription (Right x)) = toJSON x
+
+data Thumbnail = Thumbnail
+  { url    :: Text       -- ^ Url for the thumbnail.
+  , url2x  :: Maybe Text -- ^ Url for the retina version of the thumbnail.
+  , width  :: Maybe Int  -- ^ The original width of the image
+  , height :: Maybe Int  -- ^ The original height of the image
+  } deriving (Show, Eq)
+
+instance ToJSON Thumbnail where
+  toJSON (Thumbnail _url _url2x _width _height) = object
+    [ "url"    .= _url
+    , "url@2x" .= _url2x
+    , "width"  .= _width
+    , "height" .= _height
+    ]
+
+instance FromJSON Thumbnail where
+  parseJSON (Object x) = Thumbnail <$> x .: "url"
+                                   <*> x .: "url@2x"
+                                   <*> x .: "width"
+                                   <*> x .: "height"
+  parseJSON x = typeMismatch "Thumbnail" x
+
 data Card = Card
-  { style      :: CardStyle
-  , format     :: Maybe CardFormat
-  , url        :: Maybe Text
-  , title      :: Text
-  , attributes :: [CardAttribute]
-  , id         :: Text
-  , icon       :: Maybe CompoundIcon
+  { style       :: CardStyle
+  , description :: CompoundDescription
+  , format      :: Maybe CardFormat
+  , url         :: Maybe Text
+  , title       :: Text
+  , thumbnail   :: Maybe Thumbnail
+  , attributes  :: [CardAttribute]
+  , id          :: Text
+  , icon        :: Maybe CompoundIcon
   } deriving (Generic, Show)
 
 instance ToJSON Card where
